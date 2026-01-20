7debeeb27d55f9c8e4c5ff5a04a129f745f3be15
EslamElnady0 <eslamelnady254@gmail.com>
2026-01-20 14:25:30 -0800

    refactored auth with mvp and implemented the login and register presenters

--- app/src/main/java/com/mealify/mealify/presentation/auth/AuthActivity.java
+++ app/src/main/java/com/mealify/mealify/presentation/auth/AuthActivity.java
@@ -1,12 +1,9 @@
-package com.mealify.mealify.presentation.auth.views;
+package com.mealify.mealify.presentation.auth;
 
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.mealify.mealify.R;
 
