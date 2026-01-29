ed17c73f8fee9fe89418a427768f87466376dd18
EslamElnady0 <eslamelnady254@gmail.com>
2026-01-29 08:50:45 -0800

    added shared pref in login and sign up

--- app/src/main/java/com/mealify/mealify/data/auth/repo/AuthRepo.java
+++ app/src/main/java/com/mealify/mealify/data/auth/repo/AuthRepo.java
@@ -4,31 +4,88 @@ import android.content.Context;
 
 import com.mealify.mealify.core.response.GeneralResponse;
 import com.mealify.mealify.data.auth.datasources.AuthRemoteDataSource;
+import com.mealify.mealify.data.auth.datasources.PrefsDataSource;
 
 public class AuthRepo {
     private AuthRemoteDataSource authRemoteDataSource;
+    private PrefsDataSource prefsDataSource;
 
     public AuthRepo(Context context) {
-        this.authRemoteDataSource = authRemoteDataSource = AuthRemoteDataSource.getInstance(context);
+        this.authRemoteDataSource = AuthRemoteDataSource.getInstance(context);
+        this.prefsDataSource = new PrefsDataSource(context);
     }
 
     public void login(String email, String password, GeneralResponse<String> generalResponse) {
-        authRemoteDataSource.login(email, password, generalResponse);
+        authRemoteDataSource.login(email, password, new GeneralResponse<String>() {
+            @Override
+            public void onSuccess(String data) {
+                generalResponse.onSuccess(data);
+                prefsDataSource.saveString("user_id", data);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                generalResponse.onError(errorMessage);
+            }
+        });
     }
 
     public void register(String email, String password, String name, GeneralResponse<String> generalResponse) {
-        authRemoteDataSource.register(email, password, name, generalResponse);
+        authRemoteDataSource.register(email, password, name, new GeneralResponse<String>() {
+            @Override
+            public void onSuccess(String data) {
+                generalResponse.onSuccess(data);
+                prefsDataSource.saveString("user_id", data);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                generalResponse.onError(errorMessage);
+            }
+        });
     }
 
     public void googleSignIn(GeneralResponse<String> generalResponse) {
-        authRemoteDataSource.googleSignIn(generalResponse);
+        authRemoteDataSource.googleSignIn(new GeneralResponse<String>() {
+            @Override
+            public void onSuccess(String data) {
+                generalResponse.onSuccess(data);
+                prefsDataSource.saveString("user_id", data);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                generalResponse.onError(errorMessage);
+            }
+        });
     }
 
     public void signInAnonymously(GeneralResponse<String> generalResponse) {
-        authRemoteDataSource.signInAnonymously(generalResponse);
+        authRemoteDataSource.signInAnonymously(new GeneralResponse<String>() {
+            @Override
+            public void onSuccess(String data) {
+                generalResponse.onSuccess(data);
+                prefsDataSource.saveString("user_id", "guest_" + data);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                generalResponse.onError(errorMessage);
+            }
+        });
     }
 
     public void logout(GeneralResponse<String> generalResponse) {
-        authRemoteDataSource.logout(generalResponse);
+        authRemoteDataSource.logout(new GeneralResponse<String>() {
+            @Override
+            public void onSuccess(String data) {
+                generalResponse.onSuccess(data);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                generalResponse.onError(errorMessage);
+            }
+        });
     }
 }
